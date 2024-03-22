00a1c6c7456f26117604b1cc2089dbad3a1dca00
Hand <tara530991@gmail.com>
2024-03-22 11:53:33 +0800

    feature: import tailwind. adjust the height of map container

--- src/pages/Mapbox.tsx
+++ src/pages/Mapbox.tsx
@@ -1,24 +1,28 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 import mapboxgl, { Map } from "mapbox-gl";
+
+// After import mapbox-gl/dist/mapbox-gl.css, the canvas height will be 0
+// So you need to add extra css file to control the container of canvas
+import "mapbox-gl/dist/mapbox-gl.css";
+import "../css/mapbox.css"; // custom css
 
 const Mapbox = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
   const map = useRef<Map | null>(null);
-  const [lng, setLng] = useState(0);
-  const [lat, setLat] = useState(0);
-  const [zoom, setZoom] = useState(0);
 
   mapboxgl.accessToken =
-    "pk.eyJ1IjoidGFyYTUzMDk5MSIsImEiOiJjbHQ1aWF3anYwMTJiMmpwZDJ6Y3UwdWxoIn0.uNW0qpOHc9vKjkw_keWMqQ";
+    "pk.eyJ1IjoidGFyYTUzMDk5MSIsImEiOiJjbHQ1am00aXQwMXA1MmtwZGN5Y2hwb2RwIn0.HMJLTyEcK6uWT5uN7CUoTA";
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current || "",
       style: "mapbox://styles/tara530991/clu0vasea021s01o8bkq8cxl3",
-      center: [lng, lat],
-      zoom: zoom,
-      maxZoom: 3,
+      center: [0, 0],
+      zoom: 1.5,
+      maxZoom: 5,
+      minZoom: 1.5,
+      attributionControl: false,
     });
   });
 
