3244ef401aa9a3ce95b6e0a347fe6c3426ed2ed3
Hand <tara530991@gmail.com>
2024-04-22 17:47:59 +0800

    feature: calculate the country list which has nes and news list of corresponding country

--- src/pages/Mapbox.tsx
+++ src/pages/Mapbox.tsx
@@ -1,16 +1,18 @@
-import { useRef, useEffect, useState } from "react";
-import mapboxgl, { Map } from "mapbox-gl";
+import { useRef, useEffect, useState, useMemo } from "react";
+import mapboxgl, { Map as MapboxMap } from "mapbox-gl";
 import GeoCountryData from "../assets/countries.json";
 import { Geometry, GeoJsonObject } from "geojson";
+import NewsData from "../assets/mock-news.json";
 
 // After import mapbox-gl/dist/mapbox-gl.css, the canvas height will be 0
 // So you need to add extra css file to control the container of canvas
 import "mapbox-gl/dist/mapbox-gl.css";
 import "../css/mapbox.css"; // custom css
+import { useContentfulCountry } from "../hooks/useContentfulCountry";
 
 type CustomProperties = {
   name: string;
-  id: string;
+  id: string; // ISO3166 alpha3, length 3
 };
 interface CustomFeature<
   G extends Geometry | null = Geometry,
@@ -21,14 +23,56 @@ interface CustomFeature<
   id?: string | number | undefined;
   properties: P;
 }
-type CustomFeatureData = [CustomFeature, string, string];
+export type CustomFeatureData = [CustomFeature, string, string];
+export interface News {
+  id: string;
+  title: string;
+  public_time: string;
+  reporter: string;
+  source: string;
+  url: string;
+  countries: string[];
+}
 
 const Mapbox = () => {
   const mapContainer = useRef<HTMLDivElement>(null);
-  const map = useRef<Map | null>(null);
-  const geoCountry: CustomFeatureData[] = GeoCountryData[0].data
+  const map = useRef<MapboxMap | null>(null);
+  const geoCountries: CustomFeatureData[] = GeoCountryData[0].data
     .allData as unknown as CustomFeatureData[];
   const [selectedCountry, setCountry] = useState<string>("");
+
+  const newsList: News[] = NewsData as unknown[] as News[];
+
+  // let mapbox mark the highlight countries' list
+  const contentfulCountry = useMemo((): CustomFeature[] => {
+    const countryMap = new Map();
+    newsList.forEach(({ countries }: { countries: string[] }) => {
+      geoCountries
+        .filter((c) => countries.includes(c[0].properties.id))
+        .forEach((i) => {
+          const newsCountryId = i[0].properties.id;
+          if (!countryMap.get(countryMap.get(newsCountryId))) {
+            countryMap.set(newsCountryId, i[0]);
+          }
+        });
+    });
+    return [...countryMap.values()];
+  }, [newsList, geoCountries]);
+
+  const countryNewsMapping = useMemo((): { [key: string]: News[] } => {
+    return newsList.reduce((acc: { [key: string]: News[] }, n: News) => {
+      // const countryInNewsList = n.countries;
+      const accCountries: string[] = Object.keys(acc);
+      n.countries.forEach((c) => {
+        if (accCountries.some((code) => code === c)) {
+          acc[c].push(n);
+        } else {
+          acc[c] = [n];
+        }
+      });
+      return acc;
+    }, {} as { [key: string]: News[] });
+  }, [newsList]);
 
   mapboxgl.accessToken =
     "pk.eyJ1IjoidGFyYTUzMDk5MSIsImEiOiJjbHQ1am00aXQwMXA1MmtwZGN5Y2hwb2RwIn0.HMJLTyEcK6uWT5uN7CUoTA";
@@ -49,8 +93,7 @@ const Mapbox = () => {
     let hoveredPolygonId: string | number | undefined = undefined;
 
     mapInstance.on("load", () => {
-      geoCountry.forEach((item) => {
-        const f: CustomFeature = item[0];
+      contentfulCountry.forEach((f) => {
         const countryId: string = f.properties.id;
         const countryName: string = f.properties.name;
 
@@ -73,6 +116,7 @@ const Mapbox = () => {
             ],
           },
         });
+
         // When the user moves their mouse over the state-fill layer, we'll update the
         // feature state for the feature under the mouse.
         mapInstance.on("mousemove", countryName, () => {
